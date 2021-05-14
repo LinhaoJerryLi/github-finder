954e9a78c3a3bbacd4f1316da94fd604937abdcf
LinhaoJerryLi <mail@linhaoli.com>
2021-05-14 10:42:06 +0800

    14 May

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+// import proptypes by impt
+import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
+
+// move out from function
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
